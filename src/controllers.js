62c7131933f8358d10311ce57c02ba01ba02fcec
Agustín Cejas <agus782009@gmail.com>
2023-06-16 23:14:48 -0300

    Arreglo conflicto surgido al hacer fork

--- src/controllers.js
+++ src/controllers.js
@@ -15,7 +15,72 @@ router.get("/sub/:a/:b", async function (req, res) {
     } else {
         const result = core.sub(a, b);
 
-        await createHistoryEntry({ firstArg: a, operationName: "ADD" })
+        await createHistoryEntry({ firstArg: a, secondArg: b, result: result, operationName: "SUB" }) 
+        return res.send({ result });
+    }
+});
+
+
+router.get("/add/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else {
+        const result = core.add(a, b);
+
+        await createHistoryEntry({ firstArg: a, secondArg: b, result: result, operationName: "ADD" }) 
+        return res.send({ result });
+    }
+});
+
+
+
+router.get("/div/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+  
+    if (isNaN(a) || isNaN(b)) {
+      res.status(400).send('Uno de los parámetros no es un número');
+    } else if (b === 0) {
+      res.status(400).send({ error: true, message: 'No se puede dividir por cero' });
+    } else {
+      const result = core.div(a, b);
+      
+      await createHistoryEntry({ firstArg: a, secondArg: b, result: result, operationName: "DIV" });
+      return res.send({ result });
+    }
+  });
+
+router.get("/pow/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else {
+        const result = core.pow(a, b);
+
+        await createHistoryEntry({ firstArg: a, secondArg: b, result: result, operationName: "POW" });
+        return res.send({ result });
+    }
+});
+
+router.get("/mul/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else {
+        const result = core.mul(a, b);
+        
+        await createHistoryEntry({ firstArg: a, secondArg: b, result: result, operationName: "MUL" }) 
         return res.send({ result });
     }
 });
